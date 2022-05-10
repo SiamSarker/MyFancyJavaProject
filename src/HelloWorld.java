552b284272e02cdbeb52c8e9c7b917c8b3ab0285
Rajeet <rhossain192096@bscse.uiu.ac.bd>
2022-05-10 20:47:15 +0600

    Y added new methode

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,10 @@
 public class HelloWorld {
+    int add(int x, int y){
+      return (x + y);
+    }
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        int x = add(2, 3);
+        System.out.println(x);
     }
 }
